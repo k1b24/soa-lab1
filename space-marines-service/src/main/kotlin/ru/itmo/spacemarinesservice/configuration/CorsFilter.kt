c28fb50f2f23609ce18c6c43fa28043c9f44e74e
s.lazeev <s.lazeev@okko.tv>
2024-10-18 11:46:27 +0300

    add ssl + enable cors

---
+++ space-marines-service/src/main/kotlin/ru/itmo/spacemarinesservice/configuration/CorsFilter.kt
@@ -0,0 +1,14 @@
+package ru.itmo.spacemarinesservice.configuration
+
+import jakarta.ws.rs.container.ContainerRequestContext
+import jakarta.ws.rs.container.ContainerResponseContext
+import jakarta.ws.rs.container.ContainerResponseFilter
+import jakarta.ws.rs.ext.Provider
+
+@Provider
+class CorsFilter : ContainerResponseFilter {
+
+    override fun filter(requestContext: ContainerRequestContext, responseContext: ContainerResponseContext) {
+        responseContext.headers.add("Access-Control-Allow-Origin", "*")
+    }
+}
